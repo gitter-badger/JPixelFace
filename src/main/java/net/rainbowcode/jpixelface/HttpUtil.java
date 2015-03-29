16af903a488901d7e8be750c42c496bb888c1a98
Benjamin Harrison-Sims <tehrainbowguy@gmail.com>
2015-03-29 04:28:17 +0100

    Use OkHTTP

--- src/main/java/net/rainbowcode/jpixelface/HttpUtil.java
+++ src/main/java/net/rainbowcode/jpixelface/HttpUtil.java
@@ -1,68 +1,51 @@
 package net.rainbowcode.jpixelface;
 
-import org.apache.commons.io.IOUtils;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
 
 public class HttpUtil
 {
     private static final Logger LOGGER = LogManager.getLogger();
+    private static final OkHttpClient client = new OkHttpClient();
 
     public static HttpStringResponse get(String url) throws IOException
     {
-        URL obj = new URL(url);
+        Request request = new Request.Builder()
+                .url(url)
+                .addHeader("User-Agent", "jPixelFace dev")
+                .get()
+                .build();
+
         LOGGER.info("Sending 'GET' request to URL : {}", url);
-        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        Response response = client.newCall(request).execute();
 
-        // optional default is GET
-        con.setRequestMethod("GET");
-
-        //add request header
-        con.setRequestProperty("User-Agent", "jPixelFace dev");
-        int responseCode = con.getResponseCode();
+        int responseCode = response.code();
         LOGGER.info("Request to url finished: {} - Response code: {}", url, responseCode);
 
-        BufferedReader in = new BufferedReader(
-                new InputStreamReader(con.getInputStream()));
-        String inputLine;
-        StringBuilder response = new StringBuilder();
-        if (responseCode == 200)
-        {
-            while ((inputLine = in.readLine()) != null)
-            {
-                response.append(inputLine);
-            }
-        }
-
-        in.close();
-
-        return new HttpStringResponse(response.toString(), responseCode);
+        return new HttpStringResponse(response.body().string(), responseCode);
     }
 
     public static byte[] getAsBytes(String url) throws IOException
     {
-        URL obj = new URL(url);
+        Request request = new Request.Builder()
+                .url(url)
+                .addHeader("User-Agent", "jPixelFace dev")
+                .get()
+                .build();
+
         LOGGER.info("Sending 'GET' request to URL : {}", url);
-        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        Response response = client.newCall(request).execute();
 
-        // optional default is GET
-        con.setRequestMethod("GET");
-
-        //add request header
-        con.setRequestProperty("User-Agent", "jPixelFace dev");
-
-        int responseCode = con.getResponseCode();
+        int responseCode = response.code();
         LOGGER.info("Request to url finished: {} - Response code: {}", url, responseCode);
 
-
-        return IOUtils.toByteArray(con.getInputStream());
+        return response.body().bytes();
     }
 
 }
